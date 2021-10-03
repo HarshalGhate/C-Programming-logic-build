8e167e793ee9254f8a0e63ac93f30a770c534aa6
HarshalGhate <87290454+HarshalGhate@users.noreply.github.com>
2021-10-03 20:40:20 +0530

    Add files via upload

---
+++ Problems on Pattern Printing/Pattern6.cpp
@@ -0,0 +1,37 @@
+/*1. Accept number of rows and number of columns from user and display below 
+pattern. 
+Input : iRow = 4 iCol = 4 
+Output :
+ A B C D 
+ A B C D 
+ A B C D 
+ A B C D
+*/
+
+#include<iostream>
+using namespace std;
+
+void Pattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+    char ch='A';
+    for(i=1;i<=iRow;i++)
+    {
+        for ( j =1,ch='A'; j <=iCol; j++,ch++)
+        {
+            printf("%c\t",ch);
+        }
+        cout<<endl;        
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+    cout<<"Enter the number of Rows\n";
+    cin>>iValue1;
+    cout<<"Enter the number of Colums\n";
+    cin>>iValue2;
+
+    Pattern(iValue1,iValue2);
+    return 0;
+}
